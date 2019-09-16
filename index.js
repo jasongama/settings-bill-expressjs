3785475d129dedd7256a3b13a6c56674ba104c9b
Plenis <plenissino@gmail.com>
2019-09-16 18:36:17 +0200

    working progress

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require ("./settings-billLogic");
+const app = express();
+ 
+const settingsBill = SettingsBill();
+const handlebarsSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath: "./views",
+    layoutsDir: "./views/layouts"
+});
+app.engine('handlebars', handlebarsSetup);
+app.set("view engine", "Handlebars")
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static("public"))
+
+app.set('view engine', 'handlebars');
+
+app.get("/", function (req, res) {
+    res.render("index", {
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    })
+});
+
+app.post("/settings", function (req, res) {
+ 
+ settingsBill.setSettings(  {
+     callCost: req.body.callCost,
+     smsCost: req.body.smsCost,
+     warningLevel: req.body.warningLevel,
+     criticalLevel: req.body.criticalLevel
+ })
+
+
+ res.redirect("/");
+ 
+});
+
+app.post("/action", function (req, res) {
+
+    settingsBill.recordAction(req.body.actionType)
+    
+    res.redirect("/");
+});
+
+app.get("/actions", function (req, res) {
+
+});
+
+app.get("/actions/:type", function (req, res) {
+
+});
+
+let PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, function () {
+    console.log("Setting", PORT)
+});
